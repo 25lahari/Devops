31fd4247aca43b3776e3784b4c600702b341ae7f
lahari <21wh1a6652@bvrithyderabad.edu.in>
2024-06-01 11:50:20 +0530

    first_change

---
+++ profile.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class main{
+public static void main(String args[]){
+	System.out.println("NAme of the student: Lahari");
+	System.out.println("College of the student: BVRIT HYDERABAD");
+	System.out.println("Address of the student: Hyderabad");
+	System.out.println("Department of the student: CSE-AIML");
+	}
+}
